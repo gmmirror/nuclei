aebf2f14ea0648d82f4098f4376c5a94eb088fff
gmmirror <blackstar76776@gmail.com>
2026-04-02 21:29:05 +0000

    initial commit

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [[ ! -d "${PREFIX}/opt" ]]; then
+    command mkdir -p "${PREFIX}/opt"
+fi
+
+if [[ -d "${PREFIX}/opt/nuclei" ]]; then
+    command rm -rf "${PREFIX}/opt/nuclei"
+fi
+
+if [[ -x "${PREFIX}/bin/nuclei" ]]; then
+    command rm -f "${PREFIX}/bin/nuclei"
+fi
+
+command git clone --depth 1 \
+    'https://github.com/projectdiscovery/nuclei' \
+    "${PREFIX}/opt/nuclei"
+
+cd "${PREFIX}/opt/nuclei"
+command go mod tidy
+export CGO_ENABLED=1
+command go build -v -o \
+    "${PREFIX}/bin/nuclei" \
+    "cmd/nuclei/main.go"
+cd
